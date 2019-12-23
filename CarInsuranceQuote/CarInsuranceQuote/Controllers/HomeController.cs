2e9b46c946a702439e3154fdbf77ed1595b70632
Robert Cunningham <robert@bagpyp.net>
2019-12-22 16:02:49 -0800

    move estimate calculation to home controller to intercept db-write

--- CarInsuranceQuote/CarInsuranceQuote/Controllers/HomeController.cs
+++ CarInsuranceQuote/CarInsuranceQuote/Controllers/HomeController.cs
@@ -21,8 +21,7 @@ namespace CarInsuranceQuote.Controllers
             {
                 //instantiating a class object and assigning it properties based on user input
                 var customer = new Customer
-                {
-                    Id = 1,
+                {                    
                     FirstName = firstName,
                     LastName = lastName,
                     Email = email,
@@ -34,6 +33,65 @@ namespace CarInsuranceQuote.Controllers
                     Tickets = tickets,
                     Coverage = coverage
                 };
+
+                customer.Estimate = 50m;
+                //Gets customerAge
+                int customerAge = DateTime.Now.Year - Convert.ToDateTime(customer.BirthDate).Year;
+                //If the user is under 25, add $25 to the monthly total.
+                if (customerAge < 25 && customerAge > 17)
+                {
+                    customer.Estimate += 25m;
+                }
+                //If the user is under 18, add $100 to the monthly total.
+                else if (customerAge < 18)
+                {
+                    customer.Estimate += 100m;
+                }
+                //If the user is over 100, add $25 to the monthly total.
+                else if (customerAge > 100)
+                {
+                    customer.Estimate += 25m;
+                }
+                //If the car's year is before 2000, add $25 to the monthly total.
+                if (customer.CarYear < 2000)
+                {
+                    customer.Estimate += 25m;
+                }
+                //If the car's year is after 2015, add $25 to the monthly total.
+                else if (customer.CarYear > 2015)
+                {
+                    customer.Estimate += 25;
+                }
+                //If the car's Make is a Porsche, add $25 to the price.
+                if (customer.CarMake.ToLower() == "porsche")
+                {
+                    customer.Estimate += 25m;
+                }
+                //If the car's Make is a Porsche and its model is a 911 Carrera, add an additional $25 to the price.
+                if (customer.CarMake.ToLower() == "porsche" && customer.CarModel.ToLower() == "911")
+                {
+                    customer.Estimate += 25m;
+                }
+                //Add $10 to the monthly total for every speeding ticket the user has.
+                for (int i = 0; i < customer.Tickets; i++)
+                {
+                    customer.Estimate += 10m;
+                }
+                //If the user has ever had a DUI, add 25 % to the total.
+                if (customer.DUI == "true")
+                {
+                    customer.Estimate *= 1.25m;
+                }
+                //If it's full coverage, add 50% to the total.
+                if (customer.Coverage == "full")
+                {
+                    customer.Estimate *= 1.5m;
+                }
+
+                
+
+
+
 
                 //adding and saving new class object to database
                 db.Customers.Add(customer);
